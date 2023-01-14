a5f0761cb8fcf8362e02bae0ff94ddcbce5dc246
Kyle Gervers <gerverskyle@gmail.com>
2023-01-13 16:46:09 -0800

    Added git and gh as requested conda packages

--- code/00-build.sh
+++ code/00-build.sh
@@ -19,7 +19,7 @@ pheniqs=2.1.0 cutadapt=4.2 itsx=1.1.3 vsearch=2.22.1
 
 # Install packages from the conda-forge channel ####
 conda install -y -p ./env -c conda-forge r-base=4.2.2 r-renv=0.16.0 \
-pkg-config=0.29.2 libgit2=1.5.0
+pkg-config=0.29.2 libgit2=1.5.0 git=2.39.0 gh=2.21.2
 
 # Download all R packages specified in renv.lock ####
 conda run -p ./env Rscript code/00-build.r
