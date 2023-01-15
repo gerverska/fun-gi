21f890d61955f36349255c07265eede250fe453d
Kyle Gervers <gerverskyle@gmail.com>
2023-01-14 16:23:14 -0800

    Fixed environment updating behavior

--- code/00-update.sh
+++ code/00-update.sh
@@ -1,7 +1,7 @@
 # Update the conda environment and R package space according to state files ####
 
 # conda ####
-conda env update -p ./env -f conda.yml --prune
+conda env update -p ./env -f conda.yml
 
 # R ####
 R -e 'renv::restore(prompt = F)'
