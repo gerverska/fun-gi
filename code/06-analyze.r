fc51eb4f7d5bbf56966f6f06ee413b42359815a5
Kyle Gervers <gerverskyle@gmail.com>
2023-01-25 12:09:16 -0800

    Added mixed + fixed effect model testing to analysis

--- code/06-analyze.r
+++ code/06-analyze.r
@@ -1,7 +1,7 @@
 # Load packages ####
-library(patchwork)
 library(cowplot)
 library(ggplot2)
+library(nlme)
 
 # Set palette ####
 color.pal <- c('#000000', '#E69F00', '#56B4E9',
@@ -18,8 +18,9 @@ system(paste('touch', file.path(out, 'README.md')))
 fun.gi <- readRDS(file.path('05-rarefy', 'fun-gi-rare.rds'))
 
 meta <- fun.gi$meta
-meta$fun_n <- meta$fun_n |> gsub("_fun_fwd|_fun_rev", '', x =_) |> gsub('n', 'N', x = _)
-meta$gi_n <- meta$gi_n |> gsub("_gi_fwd|_gi_rev", '', x =_) |> gsub('n', 'N', x = _)
+rownames(meta) <- meta$combo
+meta$fun_n <- meta$fun_n |> gsub("_fun_fwd|_fun_rev", '', x = _) |> gsub('n', 'N', x = _)
+meta$gi_n <- meta$gi_n |> gsub("_gi_fwd|_gi_rev", '', x = _) |> gsub('n', 'N', x = _)
 
 standard <- subset(meta, template == 'standard')
 dfssmt <- subset(meta, template == 'dfssmt')
@@ -39,8 +40,39 @@ depth <- ggplot(standard, aes(x = log10(dilution), y = log10(reads)
           axis.title.y = element_text(face = 'bold'),
           legend.position = 'none')
 
-# Plot the relationship between the dilution ratio ####
-# Root-transformed fungal load ####
+# Design a full model, including frameshifts as random effects ####
+full <- lme(log10(mean_noga_load) ~ log10(dilution),
+            random = list(fun_n = ~ 1, gi_n = ~ 1),
+            data = standard,
+            method = 'ML',
+            na.action = na.omit)
+
+# Check for normally distributed residuals ####
+full |> residuals() |> qqnorm()
+full |> residuals() |> qqline()
+
+# Check for normally distributed random intercepts ####
+ranef(full)$fun_n$`(Intercept)` |> qqnorm()
+ranef(full)$fun_n$`(Intercept)` |> qqline()
+ranef(full)$gi_n$`(Intercept)` |> qqnorm()
+ranef(full)$gi_n$`(Intercept)` |> qqline()
+
+# Design a base model, only including dilution as an effect ####
+base <- lm(log10(mean_noga_load) ~ log10(dilution), standard)
+
+# Check for normally distributed residuals ####
+base |> residuals() |> qqnorm()
+base |> residuals() |> qqline()
+
+# Test whether the full and base models are similar to each other ####
+test <- anova(full, base) |> data.frame()
+
+# Add model residuals to the standard dataframe ####
+res <- data.frame(res = base$residuals)
+res$combo <- rownames(res)
+standard <- merge(standard, res, by = 'combo', all.x = T)
+
+# Plot the relationship between load and the dilution ratio ####
 load <- ggplot(standard, aes(x = log10(dilution), y = log10(mean_noga_load)
                            )) +
     geom_point(aes(color = fun_n, shape = gi_n), size = 3) +
@@ -62,26 +94,13 @@ load <- ggplot(standard, aes(x = log10(dilution), y = log10(mean_noga_load)
           axis.title.y = element_text(face = 'bold'),
           legend.title = element_text(face = 'bold'))
 
-ggplot(standard, aes(x = fun_n, y = res)) +
-    geom_jitter(aes(shape = gi_n), size = 3, width = 0.1) +
+# Plot the relationship between frameshift pairs and model residuals ####
+shifts <- ggplot(standard, aes(x = fun_n, y = gi_n, color = res)) +
+    geom_jitter(size = 3, width = 0.15, height = 0.1, alpha = 0.75) +
     xlab('\nFun frameshift') +
-    ylab('Residuals\n') +
-    labs(shape = 'Gi frameshift') +
+    ylab('Gi frameshift\n') +
+    labs(color = 'Residuals') +
     theme_cowplot() +
-    scale_color_manual(values = color.pal) +
-    theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
-          axis.title.x = element_text(face = 'bold'),
-          axis.title.y = element_text(face = 'bold'),
-          legend.title = element_text(face = 'bold'))
-
-ggplot(standard, aes(x = gi_n, y = res)) +
-    geom_jitter(aes(color = fun_n), size = 3, width = 0.1) +
-    xlab('\nGi frameshift') +
-    ylab('Residuals\n') +
-    labs(color = 'Fun frameshift') +
-    guides(color = guide_legend(order = 1)) +
-    theme_cowplot() +
-    scale_color_manual(values = color.pal) +
     theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
           axis.title.x = element_text(face = 'bold'),
           axis.title.y = element_text(face = 'bold'),
